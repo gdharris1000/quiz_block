5e88f1e08a4e9800ac6465f787940ecfa3187aa6
Gareth Harris <gdharris1000@gmail.com>
2022-09-15 20:51:58 +0100

    Show a list of Taxonomy Vocabularies in Block Form

--- src/Plugin/Block/QuizBlock.php
+++ src/Plugin/Block/QuizBlock.php
@@ -3,6 +3,7 @@
 namespace Drupal\quiz_block\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
 
 /** 
  * Quiz Block
@@ -18,6 +19,28 @@ use Drupal\Core\Block\BlockBase;
 class QuizBlock extends BlockBase {
     public function build(){
         return [];
+    }
+
+    public function blockForm($form, FormStateInterface $form_state) {
+        //Get all taxonomy terms
+        $vocabularies = \Drupal::entityTypeManager()->getStorage('taxonomy_vocabulary')->loadMultiple();
+        
+        //Get the name and ID of each term
+        $vocab_names = [];
+        foreach($vocabularies as $vocab) {
+            $name = $vocab->get('name');
+            $vid = $vocab->get('vid');
+            $vocab_names[] = [$name => $vid];
+        }
+
+        //List the available taxonomy terms
+        $form['vocabs'] = [
+            '#type' => 'select',
+            '#title' => 'Select Term',
+            '#options' => $vocab_names,
+            ];
+            
+        return $form;
     }
 
 }
